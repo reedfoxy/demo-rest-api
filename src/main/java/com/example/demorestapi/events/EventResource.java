91197419d6e92b874aaf20b31c021e1e9afee401
kbs <kbs@kbsui-iMac.local>
2019-07-16 08:11:26 +0900

    rest docs

--- src/main/java/com/example/demorestapi/events/EventResource.java
+++ src/main/java/com/example/demorestapi/events/EventResource.java
@@ -9,6 +9,7 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 @Getter
 public class EventResource extends ResourceSupport {
 
+    // jsonUnWrapped 하게되면 event : {} 형태가 아닌 원본 데이터 자체를 json 에 삽입한다.
     @JsonUnwrapped
     private Event event;
 
