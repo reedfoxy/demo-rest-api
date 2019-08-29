e6c0cf6e3d5b6d01960c12a213ec139d5888e402
bonsoo <kbs@hoyun.co.kr>
2019-08-29 17:49:38 +0900

    account service, user Details

---
+++ src/test/java/com/example/demorestapi/accounts/AccountServiceTest.java
@@ -0,0 +1,46 @@
+package com.example.demorestapi.accounts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class AccountServiceTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    public void findByUsername() {
+
+        String password = "1234";
+        String username = "admin@test.com";
+
+        Account account = Account.builder()
+                .email(username)
+                .password(password)
+                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                .build();
+        this.accountRepository.save(account);
+
+        UserDetailsService userDetailsService = (UserDetailsService)accountService;
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        assertThat(userDetails.getPassword()).isEqualTo(password);
+
+    }
+}
